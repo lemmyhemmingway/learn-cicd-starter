24ecba294adeb76249d578f253d78c2209a308f1
yasin caykur <yasincaykur@gmail.com>
2025-02-17 22:09:28 +0300

    add cover

--- internal/auth/auth_test.go
+++ internal/auth/auth_test.go
@@ -2,26 +2,23 @@ package auth
 
 import "testing"
 
-
-
 func TestGetAPIKey(t *testing.T) {
 
 	got := true
 	want := true
 
-	if (got != want) {
+	if got != want {
 		t.Fatalf("expected %v, got: %v", got, want)
 	}
 
 }
-
 
 func TestGetAPIKeyAgain(t *testing.T) {
 
 	got := true
 	want := true
 
-	if (got != want) {
+	if got != want {
 		t.Fatalf("expected %v, got: %v", got, want)
 	}
 
